6d24d7dc2347f14d7ad106226bd3669d069f79bc
nishida namiki <ymdnmk1201@gmail.com>
2026-09-16 17:19:31 +0900

    feat: Sanctumのトークン認証を実装

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,18 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\AuthController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// 認証不要のルート
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+
+// 認証が必要なルート
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
 
 Route::get('/products', [ProductController::class, 'index']);
